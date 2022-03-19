539cbc23d68c488b1c51db8f212e5784b96ddd25
mohamed elshahawy <mohamedelshahawy95@gmail.com>
2022-03-19 21:54:10 +0200

    'first'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateUserComponent } from './Components/login-page/create-user/create-user.component';
 import { DetailsForDdlComponent } from './Components/login-page/details-for-ddl/details-for-ddl.component';
 import { HomePageComponent } from './Components/login-page/home-page/home-page.component';
-import { LoginPageComponent } from './Components/login-page/login-page.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'LoginPage', component: LoginPageComponent },
+  
   { path: 'HomePage', component: HomePageComponent },
   { path: 'Details', component: DetailsForDdlComponent },
   { path: 'CreateUser', component: CreateUserComponent },
